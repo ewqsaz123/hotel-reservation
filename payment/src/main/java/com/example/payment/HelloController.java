6cc074cf7955cdd98697b9e658ce97e297b44fc9
Administrator <popuyt123@gmail.com>
2021-08-19 00:33:27 +0900

    add service

---
+++ payment/src/main/java/com/example/payment/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.payment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class HelloController {
+
+    @GetMapping
+    String HelloWorld(){
+        return "Hello payment";
+    }
+    
+}
