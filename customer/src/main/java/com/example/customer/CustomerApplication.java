38aa84b6ce2160dbe9a74ce01d44d84c4a357c86
Administrator <popuyt123@gmail.com>
2021-08-19 18:36:31 +0900

    test

--- customer/src/main/java/com/example/customer/CustomerApplication.java
+++ customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -2,11 +2,11 @@ package com.example.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+// import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+// import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+// @EnableFeignClients
 // @EnableCircuitBreaker
 public class CustomerApplication {
 
