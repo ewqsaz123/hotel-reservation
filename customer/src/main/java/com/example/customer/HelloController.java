23b836a8ff483f5ada8d214e8c638ad6dbe746bc
Administrator <popuyt123@gmail.com>
2021-08-19 16:30:59 +0900

    modify directory

---
+++ customer/src/main/java/com/example/customer/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.customer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class HelloController {
+
+    @GetMapping
+    String HelloWorld(){
+        return "Hello new customer";
+    }
+    
+}
